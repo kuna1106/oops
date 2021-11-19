a0e4c1bdc03dee299a17cc5475290e51d176f238
namnam1106 <namnam11062002@gmail.com>
2021-11-19 09:38:05 +0700

    homework5

--- src/vn/hus/homework4/MyPolynomial.java
+++ src/vn/hus/homework4/MyPolynomial.java
@@ -3,15 +3,30 @@ package vn.hus.homework4;
 public class MyPolynomial {
 	private double[] coeffs;
 	
-	public MyPolynomial(double coeffs) {
-		this.coeffs[0] = coeffs;
+	public MyPolynomial(double... coeffs) {
+		this.coeffs = coeffs;
 	}
 	
 	public int getDegree() {
-		return coeffs.length;
+		return coeffs.length - 1;
 	}
 	
 	public String toString() {
-		return "";
+		int degree = getDegree();
+		String str = "";
+		for (int i = coeffs.length - 1; i >= 0; i--) {
+		    if (degree == 0) {
+		         str += coeffs[i];
+		         break;
+		    }
+		    if (degree == 1) {
+		         str += coeffs[i] + "x +";
+		         degree--;
+		         continue;
+		    }
+		    str += coeffs[i] + "x^" + degree + "+";
+		    degree--;
+		}
+		return str;
 	}
 }
